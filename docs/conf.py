3fb6ef826ae4d43f1a028ca2290b9e49610dbec7
fcomitani <federico.comitani@gmail.com>
2022-08-04 18:47:20 -0400

    update autodoc mock import

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Mock imports
+autodoc_mock_imports = ['numpy','scipy','matplotlib','scikit-learn','scikit-network']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -61,9 +63,6 @@ html_logo = 'figs/logo.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Mock imports
-autodoc_mock_imports = ['numpy','scipy','matplotlib']
 
 def skip(app, what, name, obj, would_skip, options):
     if name == "__init__":
